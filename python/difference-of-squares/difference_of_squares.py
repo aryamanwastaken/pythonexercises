c423fc15645e0ddb422f999fa440167471c39588
aryamanwastaken <69046025+aryamanwastaken@users.noreply.github.com>
2022-10-18 23:34:31 -0700

    completed difference of squares program in python

---
+++ python/difference-of-squares/difference_of_squares.py
@@ -0,0 +1,16 @@
+from unicodedata import numeric
+
+
+def square_of_sum(number):
+    
+    return sum(range(1, number+1))**2
+
+def sum_of_squares(number):
+    
+    return sum(i*i for i in range (1, number+1))
+
+
+def difference_of_squares(number):
+    
+    return square_of_sum(number)-sum_of_squares(number)
+
